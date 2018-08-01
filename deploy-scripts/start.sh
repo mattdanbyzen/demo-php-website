fa676f34c44983f78d42621a53d719cfb5dc4dc0
mattdanbyzen <40626222+mattdanbyzen@users.noreply.github.com>
2018-08-01 21:29:41 +0100

    Update start.sh

--- deploy-scripts/start.sh
+++ deploy-scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-aws s3 cp s3://farmison-puppet/puppet-firefly.zip .
-unzip -B puppet-firefly.zip
-/opt/puppetlabs/bin/puppet apply ./AppServerSetup/FireflyApp.pp > puppet.log
+aws s3 cp s3://farmison-puppet/puppet-$APPLICATION_NAME.zip .
+unzip -B puppet-$APPLICATION_NAME.zip
+/opt/puppetlabs/bin/puppet apply ./AppServerSetup/$APPLICATION_NAME.pp > puppet.log
