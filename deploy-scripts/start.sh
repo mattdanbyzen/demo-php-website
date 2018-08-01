ee2927c2ea1c66e6715195f56eaae53993f7fc52
matt.danby <matt.danby@zeninternet.co.uk>
2018-08-01 13:11:06 +0100

    Update start script

--- deploy-scripts/start.sh
+++ deploy-scripts/start.sh
@@ -3,4 +3,4 @@
 cd /tmp
 aws s3 cp s3://farmison-puppet/puppet-firefly.zip .
 unzip -B puppet-firefly.zip
-/opt/puppetlabs/bin/puppet apply ./AppServerSetup/FireflyApp.pp
+/opt/puppetlabs/bin/puppet apply ./AppServerSetup/FireflyApp.pp > puppet.log
