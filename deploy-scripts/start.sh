5a3073b27fe5a21ac46cb218858c771189074728
Matt Danby <matt.danby@zeninternet.co.uk>
2018-08-02 15:03:05 +0100

    Updated Application name in deploy-scripts

--- deploy-scripts/start.sh
+++ deploy-scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-aws s3 cp s3://farmison-puppet/puppet-$APPLICATION_NAME.zip .
+aws s3 cp s3://farmison-puppet/puppet-Firefly$APPLICATION_NAME.zip .
 unzip -B puppet-$APPLICATION_NAME.zip
-/opt/puppetlabs/bin/puppet apply ./AppServerSetup/$APPLICATION_NAME.pp > puppet.log
+/opt/puppetlabs/bin/puppet apply ./AppServerSetup/Firefly$APPLICATION_NAME.pp > puppet.log
